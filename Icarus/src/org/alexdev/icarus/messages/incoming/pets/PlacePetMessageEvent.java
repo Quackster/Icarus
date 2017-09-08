ff576eae029b5aba58923e4185d34965de4c3a60
Alex Miller <TheAmazingAussie@users.noreply.github.com>
2017-09-09 01:40:10 +1000

    Fix bugs

--- Icarus/src/org/alexdev/icarus/messages/incoming/pets/PlacePetMessageEvent.java
+++ Icarus/src/org/alexdev/icarus/messages/incoming/pets/PlacePetMessageEvent.java
@@ -1,5 +1,6 @@
 package org.alexdev.icarus.messages.incoming.pets;
 
+import org.alexdev.icarus.game.pathfinder.Position;
 import org.alexdev.icarus.game.pets.Pet;
 import org.alexdev.icarus.game.player.Player;
 import org.alexdev.icarus.messages.MessageEvent;
@@ -16,8 +17,18 @@ public class PlacePetMessageEvent implements MessageEvent {
         
         Pet pet = player.getInventory().getPet(reader.readInt());
         
+        if (pet == null) {
+            return;
+        }
+        
         int X = reader.readInt();
         int Y = reader.readInt();
+        
+        if (X == 0 || Y == 0) {
+            Position safePosition = player.getRoom().getMapping().getRandomWalkableTile();
+            X = safePosition.getX();
+            Y = safePosition.getY();
+        }
         
         player.getRoom().addEntity(pet, X, Y, 4);
         
