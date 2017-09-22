939b292930919b99e7b6410f48d72e47df187349
Alex Miller <TheAmazingAussie@users.noreply.github.com>
2017-09-22 14:51:33 +1000

    remove debug messages

--- Icarus/src/org/alexdev/icarus/messages/incoming/messenger/MessengerRequestMessageEvent.java
+++ Icarus/src/org/alexdev/icarus/messages/incoming/messenger/MessengerRequestMessageEvent.java
@@ -4,7 +4,6 @@ import org.alexdev.icarus.dao.mysql.player.MessengerDao;
 import org.alexdev.icarus.dao.mysql.player.PlayerDao;
 import org.alexdev.icarus.game.messenger.MessengerUser;
 import org.alexdev.icarus.game.player.Player;
-import org.alexdev.icarus.log.Log;
 import org.alexdev.icarus.messages.outgoing.messenger.MessengerSendRequest;
 import org.alexdev.icarus.messages.types.MessageEvent;
 import org.alexdev.icarus.server.api.messages.ClientMessage;
@@ -19,29 +18,21 @@ public class MessengerRequestMessageEvent implements MessageEvent {
         if (username == null) {
             return;
         }
-        
-        Log.println("test 1");
 
         int userId = PlayerDao.getId(username);
 
         if (userId < 1) {
             return;
         }
-        
-        Log.println("test 2");
 
         if (player.getMessenger().hasRequest(userId)) {
             return;
         }
-        
-        Log.println("test 3");
 
         //TODO: Check if they have blocked friend requests
 
         if (MessengerDao.newRequest(player.getEntityId(), userId)) {
 
-            Log.println("test 4");
-            
             MessengerUser user = new MessengerUser(userId);
             player.getMessenger().getRequests().add(user);
 
