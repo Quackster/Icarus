43d80e401fb8871f27355ce254a4d83c439c5295
Alex Miller <TheAmazingAussie@users.noreply.github.com>
2017-10-08 17:58:08 +1000

    Add comments

--- src/org/alexdev/icarus/game/item/extradata/ExtraDataReader.java
+++ src/org/alexdev/icarus/game/item/extradata/ExtraDataReader.java
@@ -6,11 +6,24 @@ import com.google.gson.Gson;
 
 public abstract class ExtraDataReader<T> {
 
+    /**
+     * Gets the json data.
+     *
+     * @param item the item
+     * @param t the class type
+     * @return the json data
+     */
     public T getJsonData(Item item, Class<T> t) {
         T settings = new Gson().fromJson(item.getExtraData(), t);
         return settings;
     }
     
+    /**
+     * Save extra data to JSON format.
+     *
+     * @param item the item
+     * @param src the data to serialise
+     */
     public void saveExtraData(Item item, T src) {
         item.setExtraData(new Gson().toJson(src));
     }
