85a0edf7821b5196a8af530987dd2207f2ccbd22
Alex Miller <TheAmazingAussie@users.noreply.github.com>
2017-10-13 01:06:23 +1000

    retrack files

--- src/main/java/org/alexdev/icarus/game/groups/members/GroupMemberType.java
+++
@@ -1,7 +0,0 @@
-package org.alexdev.icarus.game.groups.members;
-
-public enum GroupMemberType {
-    ADMINISTRATOR,
-    MEMBER,
-    REQUEST
-}
--- src/main/java/org/alexdev/icarus/game/room/scheduler/TaskType.java
+++
@@ -1,6 +0,0 @@
-package org.alexdev.icarus.game.room.scheduler;
-
-public enum TaskType {
-    REPEAT,
-    RUN_ONCE
-}
