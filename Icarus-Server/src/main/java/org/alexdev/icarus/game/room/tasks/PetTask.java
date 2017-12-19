91657d08deada2c35fe0e079abea534088e97fc5
Alex Miller <Quackster@users.noreply.github.com>
2017-12-19 11:35:25 +1000

    Dynamic pet task scheduling

--- Icarus-Server/src/main/java/org/alexdev/icarus/game/room/tasks/PetTask.java
+++ Icarus-Server/src/main/java/org/alexdev/icarus/game/room/tasks/PetTask.java
@@ -4,9 +4,12 @@ import org.alexdev.icarus.game.pets.Pet;
 import org.alexdev.icarus.game.room.Room;
 import org.alexdev.icarus.game.room.model.RoomTile;
 import org.alexdev.icarus.game.room.scheduler.RoomTask;
+import org.alexdev.icarus.game.room.scheduler.TaskType;
 import org.alexdev.icarus.game.room.user.RoomUser;
+import org.alexdev.icarus.util.RandomInteger;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class PetTask extends RoomTask {
 
@@ -25,5 +28,8 @@ public class PetTask extends RoomTask {
             RoomUser roomUser = entity.getRoomUser();
             roomUser.walkTo(tile.getX(), tile.getY());
         }
+
+        room.getScheduler().removeTask(PetTask.class);
+        room.getScheduler().scheduleEvent(RandomInteger.getRandom(10, 30), TimeUnit.SECONDS, TaskType.REPEAT, new PetTask(room));
     }
 }
