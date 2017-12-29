f2b81db293d208d77d5b056fd813d5aa8f81ea03
Quackster <quacksterr.15@gmail.com>
2017-12-29 12:32:45 +1000

    Add safety checks for pets in room

--- Icarus-Server/src/main/java/org/alexdev/icarus/game/room/tasks/PetTask.java
+++ Icarus-Server/src/main/java/org/alexdev/icarus/game/room/tasks/PetTask.java
@@ -1,5 +1,6 @@
 package org.alexdev.icarus.game.room.tasks;
 
+import org.alexdev.icarus.game.pathfinder.Position;
 import org.alexdev.icarus.game.pets.Pet;
 import org.alexdev.icarus.game.room.Room;
 import org.alexdev.icarus.game.room.model.RoomTile;
@@ -24,12 +25,32 @@ public class PetTask extends RoomTask {
         List<Pet> pets = this.room.getEntityManager().getEntitiesByClass(Pet.class);
 
         for (Pet entity : pets) {
+            boolean sendToDoor = false;
+
+            if (entity.getRoomUser().getTile() == null) {
+                sendToDoor = true;
+            }
+
+            if (!this.room.getMapping().isTileWalkable(entity.getX(),entity.getY(), entity)) {
+                sendToDoor = true;
+            }
+
+            if (sendToDoor) {
+                Position doorLocation = this.room.getModel().getDoorLocation();
+                entity.getRoomUser().warpTo(doorLocation.getX(), doorLocation.getY(), doorLocation.getRotation());
+            }
+
             RoomTile tile = this.room.getMapping().getReachableTile(entity);
+
+            if (tile == null) {
+                continue;
+            }
+
             RoomUser roomUser = entity.getRoomUser();
             roomUser.walkTo(tile.getX(), tile.getY());
         }
 
-        room.getScheduler().removeTask(PetTask.class);
-        room.getScheduler().scheduleEvent(RandomInteger.getRandom(10, 30), TimeUnit.SECONDS, TaskType.REPEAT, new PetTask(room));
+        this.room.getScheduler().removeTask(PetTask.class);
+        this.room.getScheduler().scheduleEvent(RandomInteger.getRandom(10, 30), TimeUnit.SECONDS, TaskType.REPEAT, new PetTask(room));
     }
 }
